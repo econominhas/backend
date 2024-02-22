ba6942e743314f20735dcab4e1116a7e602c242e
Deivid Ataide <102878734+deataide@users.noreply.github.com>
2024-02-22 14:48:39 -0300

    chore: add validation on localstack/buckets.sh (#28)
    Co-authored-by: Henrique Leite <contact.henriqueleite42@gmail.com>

--- localstack/buckets.sh
+++ localstack/buckets.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-awslocal s3 mb s3://las-musas-public
-awslocal s3 mb s3://las-musas-private
+bucket1="las-musas-public"
+bucket2="las-musas-private"
+
+##buckets verification
+if awslocal s3 ls "s3://$bucket1" 2>/dev/null; then
+    echo "[INFO] $bucket1 bucket already exists"
+else 
+    awslocal s3 mb s3://$bucket1
+    echo "[INFO] $bucket1 created"
+fi
+
+if awslocal s3 ls "s3://$bucket2" 2>/dev/null; then
+    echo "[INFO] $bucket2 bucket already exists"
+else 
+    awslocal s3 mb s3://$bucket2
+    echo "[INFO] $bucket2 created"
+fi
