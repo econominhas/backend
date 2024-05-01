a777fbee52b4e75591c171682475a5834bf94a89
josemarinho <jose.silva@bandtec.com.br>
2024-04-30 23:37:22 -0300

    refactor(health): delete

--- src/health-check/health-check.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { HealthCheckController } from "./health-check.controller";
-
-@Module({
-	imports: [],
-	controllers: [HealthCheckController],
-})
-export class HealthCheckModule {}
